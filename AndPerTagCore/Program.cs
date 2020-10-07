e9ca33cc2bacd17a0b5c045b3e8b7b1a004b0570
Gummie <xavicaromiro@gmail.com>
2020-10-07 14:12:30 +0200

    Catch all not controlled exceptions

--- AndPerTagCore/Program.cs
+++ AndPerTagCore/Program.cs
@@ -1,4 +1,5 @@
 using AndPerTagCore.Forms;
+using AndPerTagCore.Utilities;
 using System;
 using System.Windows.Forms;
 
@@ -12,10 +13,17 @@ namespace AndPerTagCore
         [STAThread]
         private static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            try
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            catch (Exception e)
+            {
+                Messages.ShowErrorMessage($"Send a screen capture with this info: {e.Message} - {e.StackTrace}", "Fatal error");
+            }
         }
     }
 }
